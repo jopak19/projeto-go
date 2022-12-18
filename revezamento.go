c3bbc9cdc042d87e4ea086fa1312c67efde011d6
João Paulo <41856004+jopak19@users.noreply.github.com>
2022-12-18 11:24:12 -0300

    ordenamento correto

--- revezamento.go
+++ revezamento.go
@@ -9,12 +9,19 @@ func main() {
 	bastao := make(chan int)
 
 	go corredor(bastao, 1)
+	time.Sleep(time.Second)
 	go corredor(bastao, 2)
+	time.Sleep(time.Second)
 	go corredor(bastao, 3)
+	time.Sleep(time.Second)
 	go corredor(bastao, 4)
+	time.Sleep(time.Second)
+
 	bastao <- 1
 
 	<-bastao
+
+	fmt.Printf("Fim de corrida")
 }
 
 func corredor(bastao chan int, numero int) {
